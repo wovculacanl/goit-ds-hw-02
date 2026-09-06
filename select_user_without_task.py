a9d3b9cbc58b37015cd3c7b714518c03adb1a218
Volodymyr Vlasenko <v.vlasenko.nl@gmail.com>
2026-09-06 10:49:27 +0200

    select_user_without_task added

---
+++ select_user_without_task.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+DATABASE = "users_tasks.db"
+
+def get_users_without_tasks() -> list:
+    sql = """
+    SELECT u.id, u.fullname, u.email
+    FROM users AS u
+    LEFT JOIN tasks AS t ON u.id = t.user_id
+    WHERE t.user_id IS NULL;
+    """
+
+    with sqlite3.connect(DATABASE) as con:
+        cur = con.cursor()
+        cur.execute(sql)
+        return cur.fetchall()
+
+if __name__ == "__main__":
+    users = get_users_without_tasks()
+    if users:
+        print("Users without tasks:")
+        for user in users:
+            print(f"- ID: {user[0]} | Name: {user[1]} | Email: {user[2]}")
+    else:
+        print("No users found without tasks.")
+
+
+"""
+For DBeaver:
+
+SELECT *
+FROM users
+WHERE id NOT IN (
+    SELECT DISTINCT user_id 
+    FROM tasks 
+    WHERE user_id IS NOT NULL
+);
+
+"""
